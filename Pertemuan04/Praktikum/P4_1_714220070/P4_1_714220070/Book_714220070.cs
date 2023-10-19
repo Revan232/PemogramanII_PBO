98ea9a38d45d3db4b3bacf9677a87ee87be54632
Revan_Y_F <115602475+Revan232@users.noreply.github.com>
2023-10-19 21:51:14 +0700

    Add files via upload

---
+++ Pertemuan04/Praktikum/P4_1_714220070/P4_1_714220070/Book_714220070.cs
@@ -0,0 +1,31 @@
+﻿using P4_1_714220070;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220070
+{
+    public class Book_714220070 : Product_714220070
+    {
+        protected string pagecount;
+
+        public Book_714220070(string type, string title, string pagecount) : base(type, title)
+        {
+            this.pagecount = pagecount;
+        }
+        public string PageCount
+        {
+            get
+            {
+                return this.pagecount;
+            }
+            set
+            {
+                pagecount = value;
+            }
+        }
+
+    }
+}
---
+++ Pertemuan04/Praktikum/P4_1_714220070/P4_1_714220070/Product_714220070.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220070
+{
+    public class Product_714220070
+    {
+        private string myType;
+        private string myTitle;
+        public Product_714220070()
+        {
+
+        }
+
+        public Product_714220070(string type, string title)
+        {
+            this.myType = type;
+            this.myTitle = title;
+        }
+
+        public string Mytype
+        {
+            get { return myType; }
+            set { myType = value; }
+        }
+
+        public string MyTitle
+        {
+            get { return myTitle; }
+            set { myTitle = value; }
+        }
+    }
+}
