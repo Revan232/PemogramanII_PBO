32df01d62d3b649d3ec806cc7f7029d9dfbac674
Revan_Y_F <115602475+Revan232@users.noreply.github.com>
2023-11-08 12:16:58 +0700

    Add files via upload

---
+++ Pertemuan06/Praktikum/P6_4_714220070/P6_4_714220070/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace P6_4_714220070
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.jas = new System.Windows.Forms.CheckBox();
+            this.kebaya = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.s = new System.Windows.Forms.RadioButton();
+            this.l = new System.Windows.Forms.RadioButton();
+            this.m = new System.Windows.Forms.RadioButton();
+            this.xl = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Penyewaan Jas dan Kebaya";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(67, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(67, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nomer Telepon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(67, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 301);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 26);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Alamat";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(245, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 210);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(245, 301);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(183, 67);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(245, 168);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(183, 26);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.kebaya);
+            this.groupBox1.Controls.Add(this.jas);
+            this.groupBox1.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(539, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(148, 119);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Penyewaan";
+            // 
+            // jas
+            // 
+            this.jas.AutoSize = true;
+            this.jas.Location = new System.Drawing.Point(7, 31);
+            this.jas.Name = "jas";
+            this.jas.Size = new System.Drawing.Size(69, 30);
+            this.jas.TabIndex = 0;
+            this.jas.Text = "Jas";
+            this.jas.UseVisualStyleBackColor = true;
+            // 
+            // kebaya
+            // 
+            this.kebaya.AutoSize = true;
+            this.kebaya.Location = new System.Drawing.Point(7, 67);
+            this.kebaya.Name = "kebaya";
+            this.kebaya.Size = new System.Drawing.Size(107, 30);
+            this.kebaya.TabIndex = 1;
+            this.kebaya.Text = "Kebaya";
+            this.kebaya.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.xl);
+            this.groupBox2.Controls.Add(this.m);
+            this.groupBox2.Controls.Add(this.l);
+            this.groupBox2.Controls.Add(this.s);
+            this.groupBox2.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(539, 228);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(148, 179);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ukuran";
+            // 
+            // s
+            // 
+            this.s.AutoSize = true;
+            this.s.Location = new System.Drawing.Point(7, 35);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(47, 30);
+            this.s.TabIndex = 0;
+            this.s.TabStop = true;
+            this.s.Text = "S";
+            this.s.UseVisualStyleBackColor = true;
+            // 
+            // l
+            // 
+            this.l.AutoSize = true;
+            this.l.Location = new System.Drawing.Point(7, 110);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(48, 30);
+            this.l.TabIndex = 1;
+            this.l.TabStop = true;
+            this.l.Text = "L";
+            this.l.UseVisualStyleBackColor = true;
+            // 
+            // m
+            // 
+            this.m.AutoSize = true;
+            this.m.Location = new System.Drawing.Point(7, 74);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(52, 30);
+            this.m.TabIndex = 2;
+            this.m.TabStop = true;
+            this.m.Text = "M";
+            this.m.UseVisualStyleBackColor = true;
+            // 
+            // xl
+            // 
+            this.xl.AutoSize = true;
+            this.xl.Location = new System.Drawing.Point(7, 149);
+            this.xl.Name = "xl";
+            this.xl.Size = new System.Drawing.Size(60, 30);
+            this.xl.TabIndex = 3;
+            this.xl.TabStop = true;
+            this.xl.Text = "XL";
+            this.xl.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(245, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Pesan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(366, 456);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 35);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(67, 254);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 26);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Jenis Kelamin";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Pria",
+            "Wanita"});
+            this.comboBox1.Location = new System.Drawing.Point(245, 251);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 28);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(67, 371);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 52);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tanggal \r\nPemasanan\r\n";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(245, 381);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(183, 26);
+            this.dateTimePicker1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 586);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox kebaya;
+        private System.Windows.Forms.CheckBox jas;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton m;
+        private System.Windows.Forms.RadioButton l;
+        private System.Windows.Forms.RadioButton s;
+        private System.Windows.Forms.RadioButton xl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ Pertemuan06/Praktikum/P6_4_714220070/P6_4_714220070/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace P6_4_714220070
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "")
+            {
+                epWarning.SetError(textBox1, "Teksbox no telepon tidak boleh kosong!");
+                epWrong.SetError(textBox1, "");
+                epCorrect.SetError(textBox1, "");
+            }
+            else
+            {
+                if ((textBox4.Text).All(Char.IsNumber))
+                {
+                    epWarning.SetError(textBox4, "");
+                    epWrong.SetError(textBox4, "");
+                    epCorrect.SetError(textBox4, "Betul");
+                }
+                else
+                {
+                    epWarning.SetError(textBox4, "");
+                    epWrong.SetError(textBox4, "Imputan hanya boleh Angka !");
+                    epCorrect.SetError(textBox4, "");
+                }
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                epWarning.SetError(textBox1, "Teksbox Huruf tidak boleh kosong!");
+                epWrong.SetError(textBox1, "");
+                epCorrect.SetError( textBox1, "");
+            }
+            else
+            {
+
+                if ((textBox1.Text).All(Char.IsLetter))
+                {
+                    epWarning.SetError(textBox1, "");
+                    epWrong.SetError(textBox1, "");
+                    epCorrect.SetError(textBox1, "Betul");
+                }
+                else
+                {
+                    epWarning.SetError(textBox1 , "");
+                    epWrong.SetError(textBox1, "");
+                    epCorrect.SetError(textBox1, "Imputan hanya boleh huruf !");
+                }
+                
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                epWarning.SetError(textBox2, "Teksbox Email tidak boleh kosong!");
+                epWrong.SetError(textBox2, "");
+                epCorrect.SetError(textBox2, "");
+            }
+            else
+            {
+
+
+                if (Regex.IsMatch(textBox2.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+                {
+                    epWarning.SetError(textBox2, "");
+                    epWrong.SetError(textBox2, "");
+                    epCorrect.SetError(textBox2, "Betul!");
+                }
+                else
+                {
+                    epWarning.SetError(textBox2, "");
+                    epWrong.SetError(textBox2, "Format Email salah!\nContoh : a@b.c");
+                    epCorrect.SetError(textBox2, "");
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+
+                epWarning.SetError(textBox3, "Nama tidak boleh kosong !");
+                epWrong.SetError(textBox3, "");
+                epCorrect.SetError(textBox3, "");
+            }
+            else
+            {
+                if ((textBox3.Text).All(Char.IsUpper))
+                {
+                    epWarning.SetError(textBox3, "");
+                    epWrong.SetError(textBox3, "");
+                    epCorrect.SetError(textBox3, "Betul");
+
+                }
+                else if ((textBox3.Text).Any(char.IsLower))
+                {
+                    epWarning.SetError(textBox3, "");
+                    epWrong.SetError(textBox3, "Harus menggunakan huruf besar");
+                    epCorrect.SetError(textBox3, "");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string groupBox1 = "";
+            string groupBox2 = "";
+
+            if (jas.Checked == true)
+            {
+                groupBox1 += "Jas,";
+            }
+            if (kebaya.Checked == true)
+            {
+                groupBox1 += "kebaya,";
+            }
+            if (s.Checked)
+            {
+                groupBox2 += "Ukuran Baju anda S";
+            }
+            else if (m.Checked)
+            {
+                groupBox2 += "Ukuran Baju anda M";
+            }
+            else if (l.Checked)
+            {
+                groupBox2 += "Ukuran Baju anda L";
+            }
+            else if (xl.Checked)
+            {
+                groupBox2 += "Ukuran Baju anda XL";
+            }
+
+            if (groupBox1 != "" && groupBox2 != "")
+            {
+                string pesan = "Nama: " + textBox1.Text + "\n" +
+                           "Nomer Telepon:" + textBox4.Text + "\n" +
+                           "Email anda:" + textBox2.Text + "\n" +
+                           "Jenis Kelamin: " + comboBox1.Text + "\n" +
+                           "Alamat:" + textBox3.Text + "\n" +
+                           "Tanggal Pemesanan: " + dateTimePicker1.Text + "\n" +
+                           "Pilihan Penyewaan: " + groupBox1 + "\n" +
+                           "Pilihan Ukuran: " + groupBox2 ;
+                           ;
+                MessageBox.Show(pesan, "Informasi Pendaftaran", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("Masukkan Nama anda, tidak boleh kosong", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Masukkan Kelamin anda, tidak boleh kosong", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (groupBox1 == "")
+            {
+                MessageBox.Show("Masukkan Kelas anda, tidak boleh kosong", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (groupBox2 == "")
+            {
+                MessageBox.Show("Masukkan Jadwal anda, tidak boleh kosong", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
